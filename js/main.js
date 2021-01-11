1406e546857e0397c2eb439ba8e37982e8a7e9af
luisjulios <antonio.saurith@gmail.com>
2021-01-11 17:38:55 -0300

    Actualizando JQuery

--- js/main.js
+++ js/main.js
@@ -138,3 +138,43 @@
 
     }); //DOM CONTENT LOADED
 })();
+
+$(function() {
+    "use strict";
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+    
+    //Programa del evento
+    $('.ocultar').hide();
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+    //Animaciones para los números
+
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+    //Cuenta Regresiva
+    $('.cuenta-regresiva').countdown('2021/12/16 00:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+
+    });
+
+    return false;
+});
